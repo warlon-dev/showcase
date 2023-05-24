dba5e83ad5dd605f16df33a353a5c68b6ee2faba
warlon12 <escander.warlon@gmail.com>
2023-05-24 09:35:18 +0800

    added link for source code in Download.jsx

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -11,7 +11,10 @@ const Download = () => {
           <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
           <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
         </div>
-        <button className={styles.btnPrimary}>Source Code</button>
+        <button 
+          className={styles.btnPrimary}
+          onClick={() => window.open("https://github.com/warlon12/react-native-app-NFT", "_blank")}
+        >Source Code</button>
         <div className={styles.flexCenter}>
           <img 
             src={assets.scene}
